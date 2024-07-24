d6f2d097a5e4bd3f14166e81d3e8e65658b5f58c
Iikka Meriläinen <iikka.merilainen@rebase.fi>
2024-07-24 03:07:19 +0300

    Added db access to AzureUtil.ps1

--- AzureUtil.ps1
+++ AzureUtil.ps1
@@ -2,6 +2,10 @@ $resourceGroup = "turku-dev"
 $registry = "turkudev"
 $image = "turku-dev-servicemap-api"
 $webApp = "turku-dev-servicemap-api"
+$db = "turku-dev"
+$dbUser = "adminBHk524"
+$dbDatabase = "servicemap"
+$sshPort = 59123
 
 function Test-NetConnectionFaster($Addr, [int] $Port) {
   $TCPClient = [System.Net.Sockets.TcpClient]::new()
@@ -10,23 +14,39 @@ function Test-NetConnectionFaster($Addr, [int] $Port) {
   return $result
 }
 function Open-AzureWebAppSshConnection {
-  $command = "az webapp create-remote-connection --resource-group $resourceGroup --name $webApp -p 59123"
+  $command = "az webapp create-remote-connection --resource-group $resourceGroup --name $webApp -p $sshPort"
   $job = Start-Job -ScriptBlock { Invoke-Expression $using:command }
   return $job.Id
 }
 function Test-AzureWebAppSshConnection {
-  return Test-NetConnectionFaster 127.0.0.1 59123
+  return Test-NetConnectionFaster 127.0.0.1 $sshPort
 }
 function Open-AzureWebAppSsh {
     "Connecting to Azure WebApp... if this stalls, might need to run 'AzureUtil config ENABLE_SSH=true'"
     $jobId = Open-AzureWebAppSshConnection
-    ssh-keygen -R [localhost]:59123 >nul 2>&1
+    ssh-keygen -R [localhost]:$sshPort >nul 2>&1
     do {
         Start-Sleep -Milliseconds 10
     } until (Test-AzureWebAppSshConnection)
     "Connected, establishing SSH terminal. The password is 'Docker!'"
-    ssh root@localhost -p 59123 -o StrictHostKeyChecking=no
+    ssh root@localhost -p $sshPort -o StrictHostKeyChecking=no
     Stop-Job $jobId
+}
+
+function Open-AzurePostgresDb {
+    $myIp = Invoke-RestMethod https://api.ipify.org
+    $firewallRuleName = "Temporary_$($myIp -replace '\.', '_')"
+    "Enabling public network access to db..."
+    az postgres flexible-server update --resource-group $resourceGroup --name $db --set network.publicNetworkAccess=Enabled >$nul
+    "Whitelisting current IP in db networking..."
+    az postgres flexible-server firewall-rule create --resource-group $resourceGroup --name $db -r $firewallRuleName --start-ip-address $myIp
+    "Connecting to db..."
+    psql -h "$db.postgres.database.azure.com" -U $dbUser -d $dbDatabase
+    "Removing current IP whitelisting from db networking..."
+    az postgres flexible-server firewall-rule delete --resource-group $resourceGroup --name $db -r $firewallRuleName -y
+    "Disabling public network access to db..."
+    az postgres flexible-server update --resource-group $resourceGroup --name $db --set network.publicNetworkAccess=Disabled >$nul
+    "Done"
 }
 
 switch ($args[0]) {
@@ -35,6 +55,12 @@ switch ($args[0]) {
     }
     "log" {
         az webapp log tail --resource-group $resourceGroup --name $webApp
+    }
+    "ssh" {
+        Open-AzureWebAppSsh
+    }
+    "db" {
+        Open-AzurePostgresDb
     }
     "config" {
         switch ($args[1]) {
@@ -49,9 +75,6 @@ switch ($args[0]) {
             }
         }
     }
-    "ssh" {
-        Open-AzureWebAppSsh
-    }
     Default {
        "Usage:"
        ""
@@ -61,6 +84,8 @@ switch ($args[0]) {
        "`tView the WebApp's log stream"
        "./AzureUtil ssh"
        "`tAccess the WebApp's SSH, assuming one is set up in docker-entrypoint"
+       "./AzureUtil db"
+       "`tAccess Azure Postgres DB Flexible Server instance with psql"
        "./AzureUtil config"
        "`tShow the WebApp's environment variables in a .env file format"
        "./AzureUtil config setting1=value1 setting2=value2 ..."
